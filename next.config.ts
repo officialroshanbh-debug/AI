45a752d29b5fe3aae947499dc09dfbf6fd7006a2
Roshan Antigravity <officialroshanbh@gmail.com>
2025-12-06 18:13:11 +0545

    fix(build): remove invalid sentry option transpileClientSDK

--- next.config.ts
+++ next.config.ts
@@ -106,8 +106,7 @@ export default withSentryConfig(nextConfig, {
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
-  // Transpiles SDK to be compatible with IE11 (increases bundle size)
-  transpileClientSDK: true,
+
 
   // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
